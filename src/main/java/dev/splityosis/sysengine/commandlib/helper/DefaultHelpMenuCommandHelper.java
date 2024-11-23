f6ac500a0306e1000ed1b40b4e45d2c778da10ae
SYSPC <SplitYoSis@gmail.com>
2024-11-23 18:51:07 +0200

    Made DefaultHelpMenuCommandHelper look prettier

--- src/main/java/dev/splityosis/sysengine/commandlib/helper/DefaultHelpMenuCommandHelper.java
+++ src/main/java/dev/splityosis/sysengine/commandlib/helper/DefaultHelpMenuCommandHelper.java
@@ -2,6 +2,7 @@ package dev.splityosis.sysengine.commandlib.helper;
 
 import dev.splityosis.sysengine.commandlib.arguments.CommandArgument;
 import dev.splityosis.sysengine.commandlib.command.Command;
+import dev.splityosis.sysengine.utils.ColorUtil;
 import org.bukkit.command.CommandSender;
 
 import java.util.*;
@@ -10,19 +11,21 @@ public class DefaultHelpMenuCommandHelper extends HelpMenuCommandHelper{
 
     @Override
     public void sendHeader(CommandSender commandSender, List<Command> parentCommands, Command command, String[] args, String label) {
-        commandSender.sendMessage("=== Help Menu ===");
+        commandSender.sendMessage(ColorUtil.colorize("&cInvalid Command Usage."));
+        commandSender.sendMessage(ColorUtil.colorize("&c&m       &c Options &c&m       "));
         commandSender.sendMessage(" ");
     }
 
     @Override
     public void sendFooter(CommandSender commandSender, List<Command> parentCommands, Command command, String[] args, String label) {
         commandSender.sendMessage(" ");
-        commandSender.sendMessage("=== End of Help Menu ===");
+        commandSender.sendMessage(ColorUtil.colorize("&c&m-------------------------".replace("-", " ")));
     }
 
     @Override
     public void sendHelpLines(CommandSender commandSender, List<Command> parentCommands, Command command, String[] args, String label) {
         Set<Command> subcommands = new HashSet<>();
+        subcommands.add(command);
         command.getSubCommands().values().forEach(map -> subcommands.addAll(map.values()));
 
         if (args.length > 0)
@@ -34,12 +37,19 @@ public class DefaultHelpMenuCommandHelper extends HelpMenuCommandHelper{
                 subcommands.remove(subCommand);
             }
 
-        if (subcommands.isEmpty())
+        if (subcommands.size() == 1)
             command.getSubCommands().values().forEach(map -> subcommands.addAll(map.values()));
 
         String path = getCommandPath(parentCommands, command);
+
+        boolean first = true;
         for (Command subcommand : subcommands) {
-            StringBuilder lineBuilder = new StringBuilder("* " + path + " " + subcommand.getName() + " ");
+            StringBuilder lineBuilder = new StringBuilder("&c* " + path + " ");
+
+            if (!first) {
+                lineBuilder.append(subcommand.getName()).append(" ");
+                first = false;
+            }
 
             // Do args
             for (CommandArgument<?> argument : subcommand.getArguments())
@@ -51,7 +61,7 @@ public class DefaultHelpMenuCommandHelper extends HelpMenuCommandHelper{
             String desc = subcommand.getDescription().endsWith(".") ? subcommand.getDescription() : subcommand.getDescription() + ".";
 
             lineBuilder.append("- ").append(desc);
-            commandSender.sendMessage(lineBuilder.toString());
+            commandSender.sendMessage(ColorUtil.colorize(lineBuilder.toString()));
         }
     }
 
