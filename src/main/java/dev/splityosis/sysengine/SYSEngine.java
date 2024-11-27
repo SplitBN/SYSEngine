44abd13b9b23065d3d4ae16959e9a761d808dfa9
SYSPC <SplitYoSis@gmail.com>
2024-11-27 17:35:13 +0200

    called NBT.preloadApi() on enable

--- src/main/java/dev/splityosis/sysengine/SYSEngine.java
+++ src/main/java/dev/splityosis/sysengine/SYSEngine.java
@@ -1,5 +1,6 @@
 package dev.splityosis.sysengine;
 
+import de.tr7zw.changeme.nbtapi.NBT;
 import dev.splityosis.sysengine.configlib.ConfigLib;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -7,6 +8,12 @@ public final class SYSEngine extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        if (!NBT.preloadApi()) {
+            getLogger().warning("NBT-API wasn't initialized properly, disabling the plugin");
+            getPluginLoader().disablePlugin(this);
+            return;
+        }
+
         ConfigLib.initialize();
     }
 
