4c78e2bd32bc9fec0b2ed772d20853764a2e25c1
SYSPC <SplitYoSis@gmail.com>
2025-03-24 01:09:54 +0200

    Fixed player placeholders not working in messageAll actiontype

--- src/main/java/dev/splityosis/sysengine/actions/actiontypes/MessageAllActionType.java
+++ src/main/java/dev/splityosis/sysengine/actions/actiontypes/MessageAllActionType.java
@@ -3,6 +3,7 @@ package dev.splityosis.sysengine.actions.actiontypes;
 import dev.splityosis.sysengine.actions.ActionType;
 import dev.splityosis.sysengine.utils.ColorUtil;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
@@ -38,7 +39,11 @@ public class MessageAllActionType implements ActionType {
 
     @Override
     public void execute(Object target, @NotNull List<String> params, @NotNull Map<String, String> replacements) {
-        params = applyPlaceholders(null, params, replacements);
+        if (target instanceof Player)
+            params = applyPlaceholders((Player) target, params, replacements);
+        else
+            params = applyPlaceholders(null, params, replacements);
+
         String message = ColorUtil.colorize(params.get(0));
         Bukkit.broadcastMessage(message);
     }
